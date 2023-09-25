da445e3b1aa97461085ca5bc62e5095d85829bfc
DeveloperOscar <161139@unamba.edu.pe>
2023-09-25 10:47:59 -0500

    FEAT Agrega accion de boton por defecto onAccept
    Se quito la propiedad FocusTranversable al boton btnSelectLogo, para que
    no quitara el foco de presionar enter a btnAccept
    
    issue #21

--- src/main/java/pe/puyu/controller/UserConfigController.java
+++ src/main/java/pe/puyu/controller/UserConfigController.java
@@ -2,7 +2,6 @@ package pe.puyu.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
--- src/main/java/pe/puyu/util/JsonUtil.java
+++ src/main/java/pe/puyu/util/JsonUtil.java
@@ -1,7 +1,6 @@
 package pe.puyu.util;
 
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Optional;
